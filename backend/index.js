046d93ce08449baa18357107bdacb483dc5183b4
DiaJoshua <diajoshua05@gmail.com>
2024-10-07 19:06:57 +0800

    Changes

--- backend/index.js
+++ backend/index.js
@@ -51,7 +51,7 @@ const sendEmail = async (to, subject, text) => {
 
 
 app.use(cors({
-  origin: ['https://ecommerce-web-s55t.vercel.app'],
+  origin: 'https://ecommerce-web-s55t.vercel.app',
   credentials: true,  // This allows cookies and credentials to be sent
 }));
 app.use(express.json());
